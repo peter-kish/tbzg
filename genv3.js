5598abe1a86cc6cf34867e8f98ffabd24a9cf70a
Peter Kish <pkish023@gmail.com>
2015-08-04 16:01:42 +0200

    Added some comments for the map generation test app.

--- genv3.js
+++ genv3.js
@@ -17,12 +17,14 @@ var minMapFieldSize = 8;
 var maxMapFieldSize = 64;
 var mapFieldSizeStep = 8;
 
+// Adjust the canvas size on resize
 function onWindowResize() {
   canvas.width = window.innerWidth;
   canvas.height = window.innerHeight;
 }
 window.addEventListener("resize", onWindowResize, false);
 
+// Tile colors
 var tile_colors = [];
 tile_colors[TILE_VOID] = '#FFFFFF';
 tile_colors[TILE_WALL] = '#000000';
@@ -31,21 +33,16 @@ tile_colors[TILE_PAVEMENT] = '#BBBBBB';
 tile_colors[TILE_GRASS] = '#00BB00';
 tile_colors[TILE_HEDGE] = '#008800';
 
-//Basic logger
-var logging_enabled = true;
-function LOG(string) {
-	if (logging_enabled) {
-		console.log(string);
-	}
-}
-
+// Create the map generator
 var map = new MapGen(MAP_WIDTH, MAP_HEIGHT);
 
+// Generate a new map on click
 function onClick() {
 	map.generateMap();
 }
 canvas.addEventListener('click', onClick, false);
 
+// Zoom the map with the scroll wheel
 function onWheel(e) {
 	var delta = Math.max(-1, Math.min(1, (e.wheelDelta || -e.detail)));
 	mapFieldSize += delta * mapFieldSizeStep;
@@ -54,22 +51,22 @@ function onWheel(e) {
 }
 canvas.addEventListener('mousewheel', onWheel, false);
 
-function rgbToHex(r, g, b) {
-	return "rgb("+Math.round(r)+","+Math.round(g)+","+Math.round(b)+")";
-}
-
+// Clear the screen with white color
 function clearScreen() {
 	drawRect(0, 0, canvas.width, canvas.height, '#FFFFFF');
 }
 
+// Draw a border around the canvas
 function drawCanvasBorder() {
 	drawRectOutline(0, 0, canvas.width, canvas.height, 1, '#000000');
 }
 
+// Draw a border around the map
 function drawMapBorder(position) {
 	drawRectOutline(position.x, position.y, map.width * mapFieldSize, map.height * mapFieldSize, 1, '#000000');
 }
 
+// Draw the map
 function drawMap(position) {
 	for (var i = 0; i < map.width; i++) {
 		for (var j = 0; j < map.height; j++) {
@@ -81,27 +78,34 @@ function drawMap(position) {
 	drawMapBorder(position);
 }
 
+// Draw the map grid
 function drawMapGrid(position) {
 	var text_size = 10 * mapFieldSize / 16;
+  // Vertical lines
 	for (var i = 0; i < map.width; i++) {
 		drawText(position.x + mapFieldSize / 2 + i * mapFieldSize, position.y - mapFieldSize / 2, i, 'Calibri', text_size, GRID_COLOR, '', 'center', 'middle');
 		drawLine(position.x + i * mapFieldSize, position.y, position.x + i * mapFieldSize, position.y + MAP_HEIGHT * mapFieldSize, 1, GRID_COLOR);
 	}
+  // Horizontal lines
 	for (var i = 0; i < map.height; i++) {
 		drawText(position.x - mapFieldSize / 2, position.y + mapFieldSize / 2 + i * mapFieldSize, i, 'Calibri', text_size, GRID_COLOR, '', 'center', 'middle');
 		drawLine(position.x, position.y + i * mapFieldSize, position.x + MAP_WIDTH * mapFieldSize, position.y + i * mapFieldSize, 1, GRID_COLOR);
 	}
 }
 
+// Main loop
 function main() {
 	clearScreen();
 	//drawCanvasBorder();
 
+  // Draw with an offset so there's some place for the coordinates
 	var mapPosition = new Vector2d(mapFieldSize, mapFieldSize);
 
 	drawMap(mapPosition);
 }
 
+// Generate the map
 map.generateMap();
 
+// Redraw every 10ms
 setInterval(main, 10);
